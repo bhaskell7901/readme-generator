86724fb49d921b9dc18d41d007a3aef261a52663
Brandon Haskell <bdh747flyna1@hotmail.com>
2023-01-22 09:52:46 -0800

    updated rederlicensebadge and renderlicenselink functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -188,12 +188,14 @@ function getLicenseNames(){
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  return `[![${license}](${renderLicenseLink(license)})]` + licensesMap.get(license) === "" ? "" + `(${licensesMap.get(license)})`;
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `https://img.shields.io/static/v1?label=License&message=${license}&color=success`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
