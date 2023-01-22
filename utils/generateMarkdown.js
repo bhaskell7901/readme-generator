a74788da86037c3a762df04fcbdd50967f728c5c
Brandon Haskell <bdh747flyna1@hotmail.com>
2023-01-22 09:34:36 -0800

    updated map to only include direct link to license site

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,45 +3,49 @@ module.exports = {
   getQuestions
 }
 
-const axios = require('axios');
+const axios = require('axios');  // for GitHub login validation
 
+// Map of License name to license site (if found) for making
+// badges clickable
 const licensesMap = new Map();
-    licensesMap.set("Academic Free License v3.0", "No badge");
-    licensesMap.set("Apache license 2.0", "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
-    licensesMap.set("Artistic license 2.0", "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)");
-    licensesMap.set("Boost Software License 1.0", "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)");
-    licensesMap.set("BSD 2-clause \"Simplified\" license", "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)");
-    licensesMap.set("BSD 3-clause \"New\" or \"Revised\" license", "No badge");
-    licensesMap.set("BSD 3-clause Clear license", "No badge");
-    licensesMap.set("Creative Commons license family", "No badge");
-    licensesMap.set("Creative Commons Zero v1.0 Universal", "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)");
-    licensesMap.set("Creative Commons Attribution 4.0", "[![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)");
-    licensesMap.set("Creative Commons Attribution Share Alike 4.0", "[![License: CC BY-SA 4.0](https://licensebuttons.net/l/by-sa/4.0/80x15.png)](https://creativecommons.org/licenses/by-sa/4.0/)");
-    licensesMap.set("Do What The F*ck You Want To Public License", "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)");
-    licensesMap.set("Educational Community License v2.0", "No badge");
-    licensesMap.set("Eclipse Public License 1.0", "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)");
-    licensesMap.set("Eclipse Public License 2.0", "No badge");
-    licensesMap.set("European Union Public License 1.1", "No badge");
-    licensesMap.set("GNU Affero General Public License v3.0", "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)");
-    licensesMap.set("GNU General Public License family", "No badge");
-    licensesMap.set("GNU General Public License v2.0", "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)");
-    licensesMap.set("GNU General Public License v3.0", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)");
-    licensesMap.set("GNU Lesser General Public License family", "No badge");
-    licensesMap.set("GNU Lesser General Public License v2.1", "No badge");
-    licensesMap.set("GNU Lesser General Public License v3.0", "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)");
-    licensesMap.set("ISC", "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)");
-    licensesMap.set("LaTeX Project Public License v1.3c", "No badge");
-    licensesMap.set("Microsoft Public License", "No badge");
-    licensesMap.set("MIT", "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)");
-    licensesMap.set("Mozilla Public License 2.0", "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)");
-    licensesMap.set("Open Software License 3.0", "No badge");
-    licensesMap.set("PostgreSQL License", "No badge");
-    licensesMap.set("SIL Open Font License 1.1", "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)");
-    licensesMap.set("University of Illinois/NCSA Open Source License", "No badge");
-    licensesMap.set("The Unlicense", "No badge");
-    licensesMap.set("zLib License", "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)");
+    licensesMap.set("Academic Free License v3.0", "");
+    licensesMap.set("Apache license 2.0", "https://opensource.org/licenses/Apache-2.0)");
+    licensesMap.set("Artistic license 2.0", "https://opensource.org/licenses/Artistic-2.0");
+    licensesMap.set("Boost Software License 1.0", "https://www.boost.org/LICENSE_1_0.txt");
+    licensesMap.set("BSD 2-clause \"Simplified\" license", "https://opensource.org/licenses/BSD-2-Clause");
+    licensesMap.set("BSD 3-clause New license", "");
+    licensesMap.set("BSD 3-clause Revised license", "");
+    licensesMap.set("BSD 3-clause Clear license", "");
+    licensesMap.set("Creative Commons license family", "");
+    licensesMap.set("Creative Commons Zero v1.0 Universal", "http://creativecommons.org/publicdomain/zero/1.0/");
+    licensesMap.set("Creative Commons Attribution 4.0", "https://creativecommons.org/licenses/by/4.0/");
+    licensesMap.set("Creative Commons Attribution Share Alike 4.0", "https://creativecommons.org/licenses/by-sa/4.0/");
+    licensesMap.set("Do What The F*ck You Want To Public License", "http://www.wtfpl.net/about/");
+    licensesMap.set("Educational Community License v2.0", "");
+    licensesMap.set("Eclipse Public License 1.0", "https://opensource.org/licenses/EPL-1.0");
+    licensesMap.set("Eclipse Public License 2.0", "");
+    licensesMap.set("European Union Public License 1.1", "");
+    licensesMap.set("GNU Affero General Public License v3.0", "https://www.gnu.org/licenses/agpl-3.0");
+    licensesMap.set("GNU General Public License family", "");
+    licensesMap.set("GNU General Public License v2.0", "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html");
+    licensesMap.set("GNU General Public License v3.0", "https://www.gnu.org/licenses/gpl-3.0");
+    licensesMap.set("GNU Lesser General Public License family", "");
+    licensesMap.set("GNU Lesser General Public License v2.1", "");
+    licensesMap.set("GNU Lesser General Public License v3.0", "https://www.gnu.org/licenses/lgpl-3.0");
+    licensesMap.set("ISC", "https://opensource.org/licenses/ISC");
+    licensesMap.set("LaTeX Project Public License v1.3c", "");
+    licensesMap.set("Microsoft Public License", "");
+    licensesMap.set("MIT", "https://opensource.org/licenses/MIT");
+    licensesMap.set("Mozilla Public License 2.0", "https://opensource.org/licenses/MPL-2.0");
+    licensesMap.set("Open Software License 3.0", "");
+    licensesMap.set("PostgreSQL License", "");
+    licensesMap.set("SIL Open Font License 1.1", "https://opensource.org/licenses/OFL-1.1");
+    licensesMap.set("University of Illinois/NCSA Open Source License", "");
+    licensesMap.set("The Unlicense", "");
+    licensesMap.set("zLib License", "https://opensource.org/licenses/Zlib");
 
-const licenses = getLicenseNames();
+  // get the names as a list for inquirer questionaire
+const licenses = getLicenseNames(); 
 
 // Create an array of question's for user input
 const questions = [
@@ -183,7 +187,9 @@ function getLicenseNames(){
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
